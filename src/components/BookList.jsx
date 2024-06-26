c4d48a14512820111ac15a83c58bf885458a768d
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-26 15:25:23 +0200

    fix BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,18 +1,11 @@
 import { Card, CardBody, CardImg, CardTitle, Col, Container, Row } from "react-bootstrap";
+import SingleBook from "./SingleBook";
 
 const BookList = props => (
   <Container>
     <Row className="gy-3 mb-3">
       {props.genre.map(book => (
-        <Col key={`asin-${book.asin}`} xs={12} md={4} xl={3}>
-          <Card style={{ width: "18rem" }}>
-            <CardImg variant="top" src={book.img} />
-            <CardBody>
-              <CardTitle className="mb-3">{book.title}</CardTitle>
-              <span className="rounded-pill px-3 py-2 bg-info">${book.price}</span>
-            </CardBody>
-          </Card>
-        </Col>
+        <SingleBook book={book} />
       ))}
     </Row>
   </Container>
